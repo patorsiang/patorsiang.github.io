15b89962d6fcc9f0fd0de073ae204fd814d39201
Napatchol Thaipanich <patorseing@gmail.com>
2022-02-13 23:22:19 +0700

    chore: viewport

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -38,7 +38,7 @@ export default class MyDocument extends Document {
           <meta name="msapplication-tap-highlight" content="no" />
           <meta
             name="viewport"
-            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+            content="width=device-width, initial-scale=1.0"
           />
 
           <link
