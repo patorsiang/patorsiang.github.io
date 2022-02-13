a3b2a6a12151d2f550b95b3abef6215197889178
Napatchol Thaipanich <patorseing@gmail.com>
2022-02-13 18:38:33 +0700

    feat: add seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import type { AppProps } from "next/app";
+import NextHeadSeo from "next-head-seo";
 import { CacheProvider, EmotionCache } from "@emotion/react";
 import { ThemeProvider, CssBaseline, createTheme } from "@mui/material";
 
@@ -8,9 +9,11 @@ import "@fontsource/mali/400.css";
 import "@fontsource/mali/500.css";
 import "@fontsource/mali/700.css";
 
-import createEmotionCache from "../utility/createEmotionCache";
-import lightThemeOptions from "../styles/theme/lightThemeOptions";
-import "../styles/globals.css";
+import createEmotionCache from "@utility/createEmotionCache";
+import DEFAULT_SEO from "@utility/next-seo.config";
+
+import lightThemeOptions from "@styles/theme/lightThemeOptions";
+import "@styles/globals.css";
 interface MyAppProps extends AppProps {
   emotionCache?: EmotionCache;
 }
@@ -26,6 +29,7 @@ const MyApp: React.FunctionComponent<MyAppProps> = (props) => {
     <CacheProvider value={emotionCache}>
       <ThemeProvider theme={lightTheme}>
         <CssBaseline />
+        <NextHeadSeo {...DEFAULT_SEO} />
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
---
+++ utility/next-seo.config.ts
@@ -0,0 +1,26 @@
+export default {
+  title: "Napatchol Thaipanich's Portfolio",
+  description:
+    "Experience, Education, Skills, Projects, and Contact Information for Napatchol Thaipanich",
+  canonical: "https://patorseing.github.io/",
+  noindex: true,
+  og: {
+    url: "https://patorseing.github.io",
+    title: "Napatchol Thaipanich's Portfolio",
+    description: "Girl in IT, Software, and Web Development segment",
+    images: [
+      {
+        url: "/logo.svg",
+        width: 1200,
+        height: 630,
+        alt: "<Napatchol Thaipanich/>",
+      },
+    ],
+    site_name: "Napatchol Thaipanich's Portfolio",
+  },
+  twitter: {
+    handle: "@SeingOrPat",
+    site: "@SeingOrPat",
+    cardType: "summary_large_image",
+  },
+};
