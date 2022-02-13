446cd7f59b98aef63875a1d65124fb627a12bfc6
patorseing <patorseing@users.noreply.github.com>
2022-02-13 11:39:57 +0000

    deploy: a3b2a6a12151d2f550b95b3abef6215197889178

--- _next/static/G3PZfLLTDo-RKDkn_cocm/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/91e98dc9b9ec3ba9.css","static/chunks/pages/index-28b0f2ccb81e2492.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
