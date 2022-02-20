4c0ef9fa6f68ff9a8f3f2b30317e09fb34cc1368
Napatchol Thaipanich <patorseing@gmail.com>
2022-02-20 22:18:13 +0700

    feat: header + (cont.) footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,64 +1,135 @@
 import type { NextPage } from "next";
 
-import styles from "@styles/Home.module.css";
+import { experimentalStyled as styled } from "@mui/material/styles";
+import { Grid, Box, Link } from "@mui/material";
+
+const Item = styled(Box)(({ theme }) => ({
+  padding: theme.spacing(1),
+  textAlign: "center",
+  height: "100%",
+  lineHeight: "54px",
+}));
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{" "}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <header>
+        <Grid container sx={{ bgcolor: "secondary.main" }}>
+          <Grid item xs={1}>
+            <Item>
+              <Link
+                component="button"
+                variant="body2"
+                onClick={() => {
+                  console.info("I'm a button.");
+                }}
+                underline="none"
+                color="secondary.contrastText"
+              >
+                Home
+              </Link>
+            </Item>
+          </Grid>
+          <Grid item xs={1}>
+            <Item>
+              <Link
+                component="button"
+                variant="body2"
+                onClick={() => {
+                  console.info("I'm a button.");
+                }}
+                underline="none"
+                color="secondary.contrastText"
+              >
+                About
+              </Link>
+            </Item>
+          </Grid>
+          <Grid item xs={1}>
+            <Item>
+              <Link
+                component="button"
+                variant="body2"
+                onClick={() => {
+                  console.info("I'm a button.");
+                }}
+                underline="none"
+                color="secondary.contrastText"
+              >
+                Experience
+              </Link>
+            </Item>
+          </Grid>
+          <Grid item xs={6}>
+            <Item>
+              <Link
+                component="button"
+                variant="h3"
+                onClick={() => {
+                  console.info("I'm a button.");
+                }}
+                fontWeight="700"
+                color="primary"
+                underline="none"
+              >
+                {"<NT/>"}
+              </Link>
+            </Item>
+          </Grid>
+          <Grid item xs={2} />
+          <Grid item xs={1}>
+            <Item>
+              <Link
+                component="button"
+                variant="body2"
+                onClick={() => {
+                  console.info("I'm a button.");
+                }}
+                color="secondary.contrastText"
+                fontWeight="700"
+              >
+                Download CV
+              </Link>
+            </Item>
+          </Grid>
+        </Grid>
+      </header>
+      <footer>
+        <Grid
+          container
+          sx={{ bgcolor: "primary.contrastText", textAlign: "center" }}
         >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+          <Grid item xs={1}>
+            <Item>
+              <Link
+                component="button"
+                variant="body2"
+                onClick={() => {
+                  console.info("I'm a button.");
+                }}
+                underline="none"
+                color="secondary.contrastText"
+              >
+                napatchol.tha@gmail.com
+              </Link>
+            </Item>
+          </Grid>
+          <Grid item xs={11}></Grid>
+        </Grid>
+        <Grid
+          container
+          sx={{
+            bgcolor: "primary.contrastText",
+            fontWeight: "700",
+          }}
+        >
+          <Grid item xs={12} sx={{ textAlign: "center" }}>
+            Created by {"<Napatchol Thaipanich/> "}&copy;
+            {" " + new Date().getFullYear()}
+          </Grid>
+        </Grid>
       </footer>
-    </div>
+    </>
   );
 };
 
--- styles/theme/lightThemeOptions.ts
+++ styles/theme/lightThemeOptions.ts
@@ -1,11 +1,23 @@
-import { ThemeOptions } from '@mui/material/styles';
+import { ThemeOptions } from "@mui/material/styles";
 
 const lightThemeOptions: ThemeOptions = {
   typography: {
     fontFamily: "Mali",
   },
   palette: {
-    mode: 'light',
+    mode: "light",
+    primary: {
+      light: "#49638f",
+      main: "#1C3D73",
+      dark: "#132a50",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#f3f6f7",
+      main: "#F1F4F6",
+      dark: "#a8aaac",
+      contrastText: "#000",
+    },
   },
 };
 
