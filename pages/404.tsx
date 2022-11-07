9316da45eb501fd85ea5e891657addd4d0d70e88
Napatchol Thaipanich <patorseing@gmail.com>
2022-11-07 19:13:45 +0700

    chore: add 404 empty stage

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,18 @@
+import { Stack, Typography } from "@mui/material";
+
 // pages/NotFound.js
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <Stack height="calc(100vh - 96px - 110px)" textAlign="center">
+      <Stack my="auto" spacing={4}>
+        <Typography variant="h1">Oops!</Typography>
+        <Typography variant="h3">404 - Page Not Found</Typography>
+        <Typography variant="h6">
+          The page you were looking for was not found. <br />
+          It might have been removed, renamed, or did not exist in the first
+          place.
+        </Typography>
+      </Stack>
+    </Stack>
+  );
 }
