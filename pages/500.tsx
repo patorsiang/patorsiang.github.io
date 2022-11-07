0303087fe8df36e02640553c6137a8c3f628977c
Napatchol Thaipanich <patorseing@gmail.com>
2022-11-07 19:19:41 +0700

    chore: add 500 error stage

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,4 +1,24 @@
 // pages/ServerError.js
+import { Stack, Typography } from "@mui/material";
+import { NextSeo } from "next-seo";
+
+import DEFAULT_SEO from "@utility/next-seo.config";
+
+// pages/NotFound.js
 export default function Custom500() {
-  return <h1>500 - Server-side error occurred</h1>;
+  const { title, ...rest } = DEFAULT_SEO;
+  return (
+    <>
+      <NextSeo title="404" titleTemplate={`${title} | %s`} {...rest} />
+      <Stack height="calc(100vh - 96px - 110px)" textAlign="center">
+        <Stack my="auto" spacing={4}>
+          <Typography variant="h1">Oops!</Typography>
+          <Typography variant="h3">500 - Server-side error occurred</Typography>
+          <Typography variant="h6">
+            The owner is checking the mistake. Please come back later.
+          </Typography>
+        </Stack>
+      </Stack>
+    </>
+  );
 }
