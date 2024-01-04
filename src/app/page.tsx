a0e9bd334f8f0907690dd6403ee482e0c823355b
Napatchol Thaipanich <patorseing@gmail.com>
2024-01-04 14:53:39 +0700

    fix: next.config output

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-2x1 font-bold">My Next JS Application!</h1>
+      <h1 className="text-2x1 font-bold">
+        It is in the process to update the website
+      </h1>
     </main>
   );
 }
