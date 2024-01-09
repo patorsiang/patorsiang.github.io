d9bb78fae7adeed106bc21e237306c159375c9bc
Napatchol Thaipanich <patorseing@gmail.com>
2024-01-09 22:54:58 +0700

    chore: update cv

--- src/app/api/cv/route.ts
+++ src/app/api/cv/route.ts
@@ -50,17 +50,24 @@ type Work = {
   description: Array<string>;
 };
 
+type Activity = {
+  date: string;
+  name: string;
+  description: Array<string>;
+};
+
 const data: {
   name: string;
   address: string;
   contact: {
     [key: string]: string | { name: string; opt: pdf.TextOptions };
   };
-  info: { [key: string]: Array<University | Award | Work> };
+  info: { [key: string]: Array<University | Award | Work | Activity> };
   etc: { [key: string]: { [key: string]: string } | Array<string> };
 } = {
   name: "Napatchol Thaipanich",
-  address: "Bangkok, Thailand",
+  address:
+    "432/38 Chapter One Midtown Condo Ladprao 24, Ladprao Road, Chomphon, Chatuchak, Bangkok, Thailand 10900",
   contact: {
     Tel: "(+66)0959390164",
     Email: "napatchol.tha@gmail.com",
@@ -72,27 +79,41 @@ const data: {
         color: 0x569cd6,
       },
     },
+    Github: {
+      name: "patorseing",
+      opt: {
+        link: "https://github.com/patorseing",
+        underline: true,
+        color: 0x569cd6,
+      },
+    },
   },
   info: {
     "Education and Awards": [
-      // {
-      //   date: "2015 - 2023",
-      //   school: "Faculty of Law",
-      //   university: "Ramkhamhaeng University",
-      //   degree: "Bachelor of Laws (LL.B.)",
-      //   // gpa: "2.8",
-      //   favoriteSubjects: [
-      //     "KOR1001 - KOR2002 Fundamental Korean",
-      //     "LAW3003 Civil and Commercial Law: Family",
-      //     "LAW3009 Civil and Commercial Law: Succession",
-      //     "LAW4009 Law on Intellectual Property 1",
-      //     "LAW4010 International Trade Law",
-      //     "LAW4053 Law Related to Computer",
-      //   ],
-      // },
+      {
+        date: "2015 - 2023",
+        school: "Faculty of Law",
+        university: "Ramkhamhaeng University",
+        degree: "Bachelor of Laws (LL.B.)",
+        // gpa: "2.8",
+        favoriteSubjects: [
+          // "KOR1001 - KOR2002 Fundamental Korean",
+          // "LAW3003 Civil and Commercial Law: Family",
+          // "LAW3009 Civil and Commercial Law: Succession",
+          // "LAW4009 Law on Intellectual Property 1",
+          // "LAW4010 International Trade Law",
+          // "LAW4053 Law Related to Computer",
+          "Fundamental Korean",
+          "Civil and Commercial Law: Family",
+          "Civil and Commercial Law: Succession",
+          "Law on Intellectual Property 1",
+          "International Trade Law",
+          "Law Related to Computer",
+        ],
+      },
       {
         date: "2022",
-        name: "Women Thailand Cyber Top Talent 2022, the top 4 by NCSA & HUAWEI ",
+        name: "Women Thailand Cyber Top Talent 2022, the top 4 by NCSA & HUAWEI",
       },
       {
         date: "2020",
@@ -134,7 +155,13 @@ const data: {
         title: "Frontend Developer",
         type: "Freelance",
         location: "Bangkok, Thailand",
-        description: ["Pruxus: to maintain their website"],
+        description: [
+          "Collected requirement from a client.",
+          "Helped a client to get a solution.",
+          "Separated tasks and estimated time for each task.",
+          "Coded, Packed, Hosted, and Maintained a website.",
+          "Example: Pruxus: maintained their website, Data Wow Co., Ltd.: built a dashboard for their client.",
+        ],
       },
       {
         date: "12/2021 - 04/2023",
@@ -166,7 +193,7 @@ const data: {
         company: "Kasikorn Business Technology Group (KBTG)",
         location: "Bangkok, Thailand",
         description: [
-          "Time D(onation): It is about the use case of blockchain to solve the social problem. This project uses the Stellar blockchain to create a Time D token on the platform to exchange volunteer service and keep track of volunteer service to give a reward. I am in brainstorm the idea, design, and develop the prototype with the team",
+          "Time D(onation): It is about the use case of blockchain to solve the social problem. This project uses the Stellar blockchain to create a Time D token on the platform to exchange volunteer service and keep track of volunteer service to give a reward. I am in brainstorm the idea, design, and develop the prototype with the team.",
         ],
       },
       {
@@ -180,10 +207,45 @@ const data: {
         ],
       },
     ],
+    "Extra-Curricular Activities": [
+      {
+        date: "2020 - Current",
+        name: "Member of Sec-Girl",
+        description: [
+          "Sec-Girl is the community that includes female cyber security stakeholders in order to share knowledge and encourage women in Thailand to work in the cyber security sector.",
+          "Volunteering in a public cyber security workshop attracts 50 - 100 participants, which anyone with basic knowledge can join by registering.",
+          "Joined cyber security competitions on behalf of Sec-Girl, such as Women Thailand Cyber Top Talent 2022 by NCSA & HUAWEI.",
+        ],
+      },
+      {
+        date: "2023",
+        name: "Selected to participate in the competition, Line Hack 2023, 1 of the final 8 teams in the general public",
+      },
+      {
+        date: "2021",
+        name: 'Selected to participate in the competition, "13th Find the best telecommunications and ICT skills in honor of His Majesty", 1 of the final 30 teams in the general public',
+      },
+      {
+        date: "2018",
+        name: 'Participated in the "Group Corporate Website Design Contest Activity" by Samut Sakhon Phatthana Mueang (Social Enterprise) x MUICT',
+      },
+    ],
   },
   etc: {
+    "Computer Skills": {
+      Language:
+        "Javascript, Python, HTML, Java, PHP, TypeScript, Go (Programming Language), CSS, Tailwind CSS, SQL",
+      Framework:
+        "React.js, Vue.js, Angular.js, TypeScript, Next.js, Node.js, Express.js, UiPath",
+      Hosting:
+        "AWS Amplify, IBM Cloud, Firebase, GCP (Google Cloud Platform), Microsoft Azure",
+      Blockchain: "IBM Hyperledger Fabric, Stellar Blockchain",
+      Database:
+        "Redis, MongoDB, PostgreSQL, MSSQL, ER Diagram, Database Design",
+      "Queue Management": "RabbitMQ",
+    },
     Languages: {
-      English: "IELTS: band 5.5",
+      English: "", // "IELTS: band 5.5",
       Korean: "TOPIK: level 1",
       Thai: "mother tongue",
     },
@@ -197,7 +259,6 @@ const data: {
       "Table Tennis",
       "Badminton",
       "Swimming",
-      "ROV (MOBA game)",
     ],
   },
 };
@@ -257,7 +318,7 @@ export async function GET() {
 
   contact.forEach(([key, val], idx) => {
     addressAndContact
-      .add(typeof val === "object" ? key : "")
+      .add(typeof val === "object" ? `${key}: ` : "")
       .add(
         (typeof val === "object" ? val.name : `${key}: ${val}`) +
           (idx + 1 === contact.length ? "" : "; "),
@@ -311,7 +372,18 @@ export async function GET() {
         }
       } else if ((val as Award)?.name) {
         const awardDetail = val as Award;
-        detail.add(awardDetail.name);
+        detail.add(
+          awardDetail.name,
+          key === "Extra-Curricular Activities"
+            ? { font: fonts.HelveticaBold }
+            : {}
+        );
+        const description = (val as Activity)?.description;
+        if (description) {
+          description.forEach((desc) => {
+            detail.br().add(`- ${desc}`);
+          });
+        }
       } else {
         const { title, type, company, location, description } = val as Work;
         const workDetails = detail
@@ -340,20 +412,23 @@ export async function GET() {
     const tr = table.row();
 
     tr.cell(key, headerStyle);
-    tr.cell(
-      Array.isArray(val)
-        ? val.join(", ")
-        : Object.entries(val)
-            .map(([key, val]) => `${key}: ${val}`)
-            .join("; "),
-      dateStyle
-    );
+
+    const details = tr.cell();
+    if (Array.isArray(val)) {
+      details.text(val.join(", "), dateStyle);
+    } else {
+      Object.entries(val)
+        .map(([key, val]) => `${key}${val ? ": " + val : ""}`)
+        .forEach((val) => {
+          details.text(val, dateStyle);
+        });
+    }
   });
 
   doc.cell({ borderWidth: 0.25 });
 
   // References upon request
-  doc.text(headerStyle).add("References upon request");
+  // doc.text(headerStyle).add("References upon request");
 
   const buf = await doc.asBuffer();
 
