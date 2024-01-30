f79f48d802ba69b87bbc88195805774bcfad51b9
Napatchol Thaipanich <patorseing@gmail.com>
2024-01-30 23:16:06 +0700

    feat: 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,10 +2,12 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-center text-center gap-16">
+      <h2 className="text-7xl">Page Not Found</h2>
+      <Link className="btn btn-blue" href="/">
+        Return Home
+      </Link>
+      <h1 className="text-404 text-9xl">404</h1>
+    </main>
   );
 }
